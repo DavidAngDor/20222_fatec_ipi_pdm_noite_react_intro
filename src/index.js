d0513051112dc401bb133499b6c68adf2ac11eb8
unknown <aluno@L4M12.IPIRANGA.FATEC>
2022-08-19 21:52:57 -0300

    Aula 2 - Projeto do David

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
 
 const App = () => {
+  const estilosBotao = {marginTop: 12, paddingTop: 8, backgroundColor: 'blueviolet', paddingBottom: 8, color: 'white', border: 'none', width: '100%', borderRadius: 8}
+  const textoDoRotulo = 'Nome:'
+  const obterTextoDoBotao = () => "Enviar"
   return (
-    <div>
+    <div style={{ margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
+      <label className="rotulo" htmlFor="Nome" style={{ display: 'block', marginBottom: 4 }}>{textoDoRotulo}</label>
 
+      <input type="text" id='Nome' style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}} />
+
+      <button
+      onClick={obterTextoDoBotao} 
+      style={estilosBotao}>{obterTextoDoBotao()}</button>
     </div>
   )
 }
 
 ReactDOM.render(
   <App />,
-  document.querySelector('#root')  
+  document.querySelector('#root')
 )
